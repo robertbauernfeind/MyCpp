8f8eb73c71650dde27394130e48126d70e72e785
Robert Bauernfeind <robertbauernfeind2@gmail.com>
2025-03-18 14:25:14 +0100

    Section 2 streams - converting to string and parsing strings

--- Section 2/MyCpp2.StreamsAndFiles/main.cpp
+++ Section 2/MyCpp2.StreamsAndFiles/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iomanip>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -13,10 +14,20 @@ struct Movie {
 };
 
 void readingFromStreams();
+
 void writingToTextFiles();
+
 void readingFromTextFiles();
+
 void writingToBinaryFiles();
+
 void readingFromBinaryFiles();
+
+void workingWithFileStreams();
+
+void convertingValuesToStrings();
+
+void parsingStrings();
 
 int main() {
     // readingFromStreams();
@@ -37,6 +48,19 @@ int main() {
 
     readingFromBinaryFiles();
 
+    cout << endl;
+
+    // workingWithFileStreams();
+
+    cout << endl;
+
+    convertingValuesToStrings();
+
+    cout << endl;
+
+    parsingStrings();
+
+    cout << endl;
     return 0;
 }
 
@@ -140,6 +164,8 @@ void readingFromTextFiles() {
 }
 
 void writingToBinaryFiles() {
+    // Writing to binary files
+    cout << endl << " == Writing to binary files == " << endl;
     int numbers[] = {1'000'000, 2'000'000, 3'000'000};
 
     // Text file
@@ -159,20 +185,112 @@ void writingToBinaryFiles() {
     // 12 => 3 * 4 bytes, 3* 4 integers
     ofstream file("numbers.dat", ios::binary);
     if (file.is_open()) {
-        file.write(reinterpret_cast<char*>(&numbers), sizeof(numbers));
+        file.write(reinterpret_cast<char *>(&numbers), sizeof(numbers));
         file.close();
     }
 }
 
 void readingFromBinaryFiles() {
+    // Reading from binary files
+    cout << endl << " == Reading from binary files == " << endl;
+
     int numbers[3];
 
     ifstream file("numbers.dat", ios::binary);
     if (file.is_open()) {
         int number;
-        while (file.read(reinterpret_cast<char*>(&number), sizeof(number))) {
+        while (file.read(reinterpret_cast<char *>(&number), sizeof(number))) {
             cout << number << endl;
         }
         file.close();
     }
+}
+
+void workingWithFileStreams() {
+    // Working with file streams
+    cout << endl << " == Working with file streams == " << endl;
+
+    // difference from `fstream` to `ifsteam` and `ofstream`
+    // is, that it can be used for both, reading and writing
+    // to the same file by specifying the mode
+    fstream file;
+
+    // app => append
+    file.open("file.txt", ios::in | ios::out | ios::app);
+
+    if (file.is_open()) {
+        file.close();
+    }
+}
+
+// Execise: convert a double to a string
+// string dtos(double number) {
+//     ostringstream oss;
+//     oss << fixed << setprecision(1) << number;
+//     return oss.str();
+// }
+
+// overloading to_string()
+string to_string(double number, int precision = 1) {
+    ostringstream oss;
+    oss << fixed << setprecision(precision) << number;
+    return oss.str();
+}
+
+void convertingValuesToStrings() {
+    // Converting values to strings
+    cout << endl << " == Converting values to strings == " << endl;
+
+    double number = 12.34;
+    ostringstream stream;
+
+    // fixed: fixed point notation
+    // setprecision: number of decimal places
+    // stream << fixed << setprecision(1) << number;
+
+    // string str = to_string(number);
+    // string str = stream.str();
+    string str = to_string(number, 2);
+    cout << str << endl;
+}
+
+void parsingStrings() {
+    // Parsing Strings
+    cout << endl << " == Parsing Strings == " << endl;
+
+    string str = "10 20";
+    stringstream stream(str);
+
+    int first;
+    stream >> first;
+
+    int second;
+    stream >> second;
+
+    cout << first << endl;
+    cout << second << endl;
+
+    // Exersice: parse to a Movie struct
+    // Terminator 1, 1984
+
+    string movieStr = "Terminator 1, 1984";
+    stringstream movieStream(movieStr);
+    // while (!movieStream.eof()) {
+    //     string token;
+    //     getline(movieStream, token, ',');
+    //     Movie movie;
+    //     movie.title = token;
+    //
+    //     getline(movieStream, token);
+    //     movie.year = stoi(token);
+    //
+    //     cout << movie.title << endl;
+    // }
+
+    Movie movie;
+    getline(movieStream, movie.title, ',');
+    movieStream >> movie.year;
+
+    cout << movie.title << endl;
+    cout << movie.year << endl;
 }
